31124c586429ac5f4e43f71e2b264ef1badcd34a
Franklin Sayago <franklin.solandy@gmail.com>
2025-01-28 10:39:58 -0500

    Adding Files

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://neondb_owner:GBYUhV93gPwC@ep-wispy-art-a4rjnsko.us-east-1.aws.neon.tech/neondb?sslmode=require')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.urandom(24)  # or another method to set a secret key
+	
+	
+
+
